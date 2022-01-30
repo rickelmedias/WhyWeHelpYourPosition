f5067ab5296cd118f6aded6d64da55e4c2c8afd2
Rickelme Dias <43411893+RickelmeDias@users.noreply.github.com>
2022-01-30 20:04:07 -0300

    Update the spec.ts
    Suggested by https://github.com/Raymw1

--- src/app.spec.ts
+++ src/app.spec.ts
@@ -1,31 +1,25 @@
 const request = require('supertest');
 import { app } from './app';
 
-describe("POST /findme" , () => {
-    describe("Sending all informations correctly", () => {
-        it("should return status 200", async () => {
-            const res = await request(app).post("/findme").send(
-                {
+const OBJECT_TEST_API = {
                     name: "Rick",
                     email: "email@email.com",
                     first_keyword: "Rickelme",
                     second_keyword: "Dias",
                     website_url: "rickelmedias.dev"
-                });
+};
+
+describe("POST /findme" , () => {
+    describe("Sending all informations correctly", () => {
+        it("should return status 200", async () => {
+            const res = await request(app).post("/findme").send(OBJECT_TEST_API);
             expect(res.statusCode).toEqual(200);
         });
     });
 
     describe("Not sending the name", () => {
         it("should return the error message to name field (empty field)", async () => {
-            const res = await request(app).post("/findme").send(
-                {
-                    name: "",
-                    email: "email@email.com",
-                    first_keyword: "Rickelme",
-                    second_keyword: "Dias",
-                    website_url: "rickelmedias.dev"
-                });
+            const res = await request(app).post("/findme").send({...OBJECT_TEST_API, name: ""});
 
             expect(res.error.text).toEqual('{"name":{"error":"You must define the name.","field_type":"<string>"}}');
         });
@@ -33,14 +27,7 @@ describe("POST /findme" , () => {
 
     describe("Not sending the email field (empty field)", () => {
         it("should return the error message to email field", async () => {
-            const res = await request(app).post("/findme").send(
-                {
-                    name: "Rick",
-                    email: "",
-                    first_keyword: "Rickelme",
-                    second_keyword: "Dias",
-                    website_url: "rickelmedias.dev"
-                });
+            const res = await request(app).post("/findme").send({...OBJECT_TEST_API, email: ""});
                 
             expect(res.error.text).toEqual('{"email":{"error":"You must define the email.","field_type":"<string>"}}');
         });
@@ -48,14 +35,7 @@ describe("POST /findme" , () => {
 
     describe("Not sending the first_keyword field (empty field)", () => {
         it("should return the error message to email field", async () => {
-            const res = await request(app).post("/findme").send(
-                {
-                    name: "Rick",
-                    email: "email@email.com",
-                    first_keyword: "",
-                    second_keyword: "Dias",
-                    website_url: "rickelmedias.dev"
-                });
+            const res = await request(app).post("/findme").send({...OBJECT_TEST_API, first_keyword: ""});
 
             expect(res.error.text).toEqual('{"first_keyword":{"error":"You must define the first keyword.","field_type":"<string>"}}');
         });
@@ -63,14 +43,7 @@ describe("POST /findme" , () => {
 
     describe("Not sending the second_keyword field (empty field)", () => {
         it("should return status 200 because the second_keyword is not an obligation", async () => {
-            const res = await request(app).post("/findme").send(
-                {
-                    name: "Rick",
-                    email: "email@email.com",
-                    first_keyword: "Rickelme",
-                    second_keyword: "",
-                    website_url: "rickelmedias.dev"
-                });
+            const res = await request(app).post("/findme").send({...OBJECT_TEST_API, second_keyword: ""});
 
                 expect(res.statusCode).toEqual(200);
         });
@@ -78,14 +51,7 @@ describe("POST /findme" , () => {
     
     describe("Sending other data type for second_keyword field", () => {
         it("should return the data type error message to second_keyword field", async () => {
-            const res = await request(app).post("/findme").send(
-                {
-                    name: "Rick",
-                    email: "email@email.com",
-                    first_keyword: "Rickelme",
-                    second_keyword: 123,
-                    website_url: "rickelmedias.dev"
-                });
+            const res = await request(app).post("/findme").send({...OBJECT_TEST_API, second_keyword: 123});
 
             expect(res.error.text).toEqual('{"second_keyword":{"error":"Unexpected data type.","field_type":"<string>"}}');
         });
@@ -93,14 +59,7 @@ describe("POST /findme" , () => {
     
     describe("Not sending the website_url field (empty field)", () => {
         it("should return the error message to email field", async () => {
-            const res = await request(app).post("/findme").send(
-                {
-                    name: "Rick",
-                    email: "email@email.com",
-                    first_keyword: "Rickelme",
-                    second_keyword: "Dias",
-                    website_url: ""
-                });
+            const res = await request(app).post("/findme").send({...OBJECT_TEST_API, website_url: ""});
 
             expect(res.error.text).toEqual('{"website_url":{"error":"You must define the website.","field_type":"<string>"}}');
         });
